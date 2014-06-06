ebfc602b77edb5d62979a130307bae83d51a114f
Ahmed Shabbir <Shabbir10314@gmail.com>
2014-06-05 23:00:21 -0400

    added ignore

--- summerTest.rb
+++ summerTest.rb
@@ -1,9 +1,27 @@
 require 'ots'
 require 'open-uri'
 
+arr = Array.new
 
+if File.file?('test.txt')
+  
+else
+  f = open('http://icourse.cuc.edu.cn/computernetworks/labs/alice.txt').read
+  File.open("test.txt", 'w') { |file| file.write(f) }
+  puts 'file saved'
+end
+  
+  
+File.readlines("test.txt").each  do |line| 
+  article = OTS.parse(line)
+  a = article.summarize(percent: 25)
+  a.each do |x|
+    arr.push x[:sentence]
+  end
+end
 
-response = open('http://icourse.cuc.edu.cn/computernetworks/labs/alice.txt').each_line { |line| 
-    article = OTS.parse(line)
-    puts article.summarize(percent: 50)
-}
+File.delete("converted.txt")
+File.open("converted.txt", "w+") do |f|
+  f.puts(arr)
+end 
+
