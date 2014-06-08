8aeca2a825482a3a314e7f76b0867aeebad4b8e0
Carlos Espejo <carlosespejo@gmail.com>
2014-06-08 15:45:43 -0400

    removed white spaces

--- summer_test.rb
+++ summer_test.rb
@@ -10,23 +10,23 @@ unless File.file?('test.txt')
   f = open('http://icourse.cuc.edu.cn/computernetworks/labs/alice.txt').read
   File.open("test.txt", 'w') { |file| file.write(f) }
 end
-  
- 
+
+
 
 File.open("converted.htm", "w") do |fileHtml|
   fileHtml.puts "<HTML>"
   fileHtml.puts "<BODY>"
-end 
- 
-  
-File.readlines("test.txt").each  do |line| 
+end
+
+
+File.readlines("test.txt").each  do |line|
   article = OTS.parse(line)
   a = article.summarize(percent: 25)
   a.each do |x|
     if x[:score].to_i > 60
-      arr.push "<span class='highlighted'>" + x[:sentence] + "</span>" 
-    else 
-      arr.push  x[:sentence] 
+      arr.push "<span class='highlighted'>" + x[:sentence] + "</span>"
+    else
+      arr.push  x[:sentence]
     end
   end
 end
@@ -36,11 +36,11 @@ puts "summerized"
 
 File.open("converted.htm", "a") do |f|
   f.puts(arr)
-end 
+end
 
 File.open("converted.htm", "a") do |fileHtml|
   fileHtml.puts "</BODY></HTML>"
-end 
+end
 
 puts 'done'
 
